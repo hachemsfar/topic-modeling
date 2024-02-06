1d7501d87111608b168b7d6e71e92fcc399fde52
Hachem Sfar <35065444+hachemsfar@users.noreply.github.com>
2024-02-06 10:35:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -34,8 +34,7 @@ def main():
         st.write("Topic Probabilities:", new_probs)
 
         df=loaded_topic_model.get_topic_info()
-        # Show topics for new data
-        print(df[df['Topic']==new_topics['value'].tolist()[0]])
+
 
 if __name__ == "__main__":
     main()
